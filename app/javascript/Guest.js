1aac0903eb20dbcfd6b74a5f3acfd22b6ec496ec
Peter Lemiszki <plemiszki@gmail.com>
2025-06-11 16:53:17 -0400

    add thank you state

--- app/javascript/Guest.js
+++ app/javascript/Guest.js
@@ -1,14 +1,29 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const Guest = () => {
-    return (
-        <div className="guest">
-          <h1 className="guest__title">Welcome</h1>
-          <p className="guest__subtitle">
-            Please provide your full name and phone number.
-          </p>
-        </div>
-      )
-}
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [showThankYou, setShowThankYou] = useState(true);
 
-export default Guest
+  if (showThankYou) {
+    return (
+      <div className="guest">
+        <h1 className="guest__title">Thank You!</h1>
+        <p className="guest__subtitle">
+          Thank you for submitting the form {name}.
+        </p>
+      </div>
+    );
+  }
+
+  return (
+    <div className="guest">
+      <h1 className="guest__title">Welcome</h1>
+      <p className="guest__subtitle">
+        Please provide your full name and phone number.
+      </p>
+    </div>
+  );
+};
+
+export default Guest;
