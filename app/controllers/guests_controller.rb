513911e55f6bef5bb0ff6e6e4e29fc56d623290f
Peter Lemiszki <plemiszki@gmail.com>
2025-06-11 20:29:59 -0400

    controller

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -2,6 +2,20 @@
 
 class Api::V1::GuestsController < Api::V1::BaseController
   def create
-    head :created
+    customer = Customer.new(customer_params)
+    if customer.save
+      head :created
+    else
+      errors_hash = customer.errors.as_json(full_messages: true)
+      render json: {
+        errors: errors_hash.deep_transform_keys { |k| k.to_s.camelize(:lower) }
+      }, status: 400
+    end
+  end
+
+  private
+
+  def customer_params
+    params[:customer].permit(:name, :phone)
   end
 end
