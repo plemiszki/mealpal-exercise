2286cd85b2254759f8d15b469771afa4ff85ceac
Peter Lemiszki <plemiszki@gmail.com>
2025-06-11 20:14:31 -0400

    customer model tests

--- Gemfile
+++ Gemfile
@@ -71,4 +71,6 @@ group :test do
   gem "capybara"
   gem "selenium-webdriver"
   gem "webdrivers"
+  gem "rspec-rails"
+  gem "rails-controller-testing"
 end
---
+++ spec/models/customer_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe Customer do
+  it 'creates a customer' do
+    @customer = Customer.create!(name: "Elwood Blues", phone: "5556345789")
+    expect(Customer.count).to eq 1
+    expect(@customer.name).to eq "Elwood Blues"
+  end
+
+  it 'requires a name' do
+    expect do
+      Customer.create!(name: "", phone: "5556345789")
+    end.to raise_error(ActiveRecord::RecordInvalid)
+  end
+
+  it 'requires a phone number' do
+    expect do
+      Customer.create!(name: "Elwood Blues", phone: "")
+    end.to raise_error(ActiveRecord::RecordInvalid)
+  end
+
+  it 'requires a phone number of at least 10 digits' do
+    expect do
+      Customer.create!(name: "Elwood Blues", phone: "555634578")
+    end.to raise_error(ActiveRecord::RecordInvalid)
+  end
+
+  it 'requires a phone number no greater than 11 digits' do
+    expect do
+      Customer.create!(name: "Elwood Blues", phone: "115556345789")
+    end.to raise_error(ActiveRecord::RecordInvalid)
+  end
+end
