9c3821ca0024992aefd3bef26a089fd4cc8b08e8
Peter Lemiszki <plemiszki@gmail.com>
2025-06-11 22:05:01 -0400

    error styling wip

--- app/javascript/controllers/react_controller.js
+++ app/javascript/controllers/react_controller.js
@@ -2,11 +2,37 @@ import { Controller } from "@hotwired/stimulus";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Guest from "../Guest";
+import { ThemeProvider, createTheme } from "@mui/material";
 
 // Connects to data-controller="react"
 export default class extends Controller {
   connect() {
     const root = ReactDOM.createRoot(document.getElementById("app"));
-    root.render(<Guest />);
+    root.render(
+      <ThemeProvider
+        theme={createTheme({
+          typography: {
+            // h1: {
+            //   fontSize: "1.5rem",
+            //   fontWeight: 600,
+            // },
+            // h6: {
+            //   fontSize: "0.9rem",
+            //   fontWeight: 600,
+            // },
+            // bold: {
+            //   fontWeight: 700,
+            // },
+            error: {
+              fontSize: 14,
+              fontWeight: 500,
+              color: "rgb(211, 47, 47)",
+            },
+          },
+        })}
+      >
+        <Guest />
+      </ThemeProvider>
+    );
   }
 }
